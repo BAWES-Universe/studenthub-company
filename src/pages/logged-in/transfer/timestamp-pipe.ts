4ac8cfde00184951d6a01ab7b9069a418a8eeece
Sai Babu Batta <sbatta@mss.local>
2017-03-30 15:20:34 +0530

    View Transfer Invoices + People paid in that invoice

--- src/pages/logged-in/transfer/timestamp-pipe.ts
+++ src/pages/logged-in/transfer/timestamp-pipe.ts
@@ -5,17 +5,18 @@ import * as _ from 'lodash'
 })
 export class SortPipe implements PipeTransform {
     transform(array: Array<any>, args?: any): any {
-        if (array != undefined)
+        if (array != undefined) {
             array.sort((a: any, b: any) => {
-                if (a[args] < b[args]) {
+                if (a[args].replace(/[-:]/g, '') < b[args].replace(/[-:]/g, '')) {
                     return 1;
-                } else if (a[args] > b[args]) {
+                } else if (a[args].replace(/[-:]/g, '') > b[args].replace(/[-:]/g, '')) {
                     return -1;
                 } else {
                     return 0;
                 }
             });
-        return array;
+            return array;
+        }
     }
 }
 
