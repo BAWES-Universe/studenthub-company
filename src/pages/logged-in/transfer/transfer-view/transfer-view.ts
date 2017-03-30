a8ca87974cabe1a858ce69c5accf65c9ca1b2ea0
Sai Babu Batta <sbatta@mss.local>
2017-03-30 15:41:15 +0530

    Transfer Locked

--- src/pages/logged-in/transfer/transfer-view/transfer-view.ts
+++ src/pages/logged-in/transfer/transfer-view/transfer-view.ts
@@ -57,7 +57,17 @@ export class TransferViewPage {
     });
   }
 
- 
+  /**
+  * Transfer Locking  */
+  transferLock(invoice_id: number) {
+    // Load list of transfer
+    let loader = this._loadingCtrl.create();
+    loader.present();
+    this.transferService.makeTransfertoLock(invoice_id).subscribe(response => {
+      this.navCtrl.pop();
+      loader.dismiss();
+    });
+  }
 
   // Calculating Total cost     
   totalCost(hourly_rate, hours, bonus, transfer_cost) {
