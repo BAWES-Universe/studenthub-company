bd9549907ee6394f7911ab2135f710cdbf35e8b5
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-06-09 19:36:23 +0530

    model refactor

--- src/models/candidate.ts
+++ src/models/candidate.ts
@@ -1,25 +1,34 @@
+import { Store } from './store';
+import { University } from './university';
+import { Country } from './country';
 
 export class Candidate {
     candidate_id: number;
+    candidate_uid: string;
     store_id: number;
-    store_name: string;
     bank_id: number;
     university_id: number;
     country_id: number;
-    age: number;
-    company_name: string;
     bank_account_name: string;
     candidate_iban: string;
     candidate_name: string;
     candidate_name_ar: string;
+    candidate_personal_photo: string;
     candidate_email: string;
-    candidate_password_hash: string;
     candidate_phone: string;
+    candidate_address_line1: string;
     candidate_birth_date: string;
-    candidate_civil_id: string;
+    candidate_civil_id: number;
     candidate_civil_expiry_date: string;
     candidate_civil_photo_front: string;
     candidate_civil_photo_back: string;
-    candidate_hourly_rate: string;
-    candidate_status: string;
+    candidate_hourly_rate: number;
+    candidate_auth_key: string;
+    candidate_password_hash: string;
+    candidate_password_reset_token: string;
+    candidate_status: number;
+    approved: number;
+    store: Store;
+    university: University;
+    country: Country;
 }
