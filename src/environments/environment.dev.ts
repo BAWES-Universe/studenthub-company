f9ebf420329e0fe7ff3d446d97d9fcf76b82b099
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-07-14 16:52:30 +0530

    end points changes done

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,7 +2,7 @@
 export const environment = {
     production: false,
     envName: 'dev',
-    apiEndpoint: 'http://payroll-company.dev.studenthub.co/v1',
+    apiEndpoint: 'http://employer.api.dev.studenthub.co/v1',
     environmentName: 'Dev Server',
     serviceWorker: true
 };
