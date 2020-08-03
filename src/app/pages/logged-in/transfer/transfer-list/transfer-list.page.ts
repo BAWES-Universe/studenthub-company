b7bd358897c63cd76c7b538202953217e4484adb
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-08-03 13:00:59 +0530

    [Finishes #174088207] replaced manual pagination with infinite scroller

--- src/app/pages/logged-in/transfer/transfer-list/transfer-list.page.ts
+++ src/app/pages/logged-in/transfer/transfer-list/transfer-list.page.ts
@@ -79,6 +79,40 @@ export class TransferListPage implements OnInit {
     );
   }
 
+  /*
+ * Method perform infinite scroll which
+ * will load more data just like pagination
+ */
+  doInfinite(infiniteScroll) {
+    this.currentPage++;
+    this.loading = true;
+
+    this.transferService.list(this.currentPage).subscribe(response => {
+
+        this.pageCount = response.headers.get('X-Pagination-Page-Count');
+        this.currentPage = response.headers.get('X-Pagination-Current-Page');
+
+        this.pages = [];
+
+        for (let i = 1; i <= this.pageCount; i++) {
+          this.pages.push(i);
+        }
+
+        // hide if page = 1
+        if (this.pageCount == 1) {
+          this.pages = [];
+        }
+
+        this.transfers = response.body;
+        this.organiseTransfers();
+
+      },
+      error => { },
+      () => {
+        infiniteScroll.complete();
+      });
+  }
+
   /**
    * Organise the transfers into groups based on transfer status
    */
