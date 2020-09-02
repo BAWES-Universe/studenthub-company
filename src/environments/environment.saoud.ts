04ae77d30de4bcdd63c9d776efb2a80413450558
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-09-02 12:20:37 +0530

    [Finishes #174596278] change dev and local cloudinary path

--- src/environments/environment.saoud.ts
+++ src/environments/environment.saoud.ts
@@ -3,7 +3,7 @@ export const environment = {
     envName: 'saoud',
     apiEndpoint: 'http://localhost/~Saoud/pogi/pogi/admin/web/v1',
     permanentBucketUrl: "https://studenthub-uploads-dev-server.s3.amazonaws.com/",
-    cloudinaryUrl: 'https://res.cloudinary.com/studenthub/image/upload/c_thumb,w_200,h_200,g_face/v1596525812/candidate-photo/',
+    cloudinaryUrl: 'https://res.cloudinary.com/studenthub/image/upload/c_thumb,w_200,h_200,g_face/v1596525812/candidate-photo/dev/',
     environmentName: 'Saoud Local Machine',
     s3Domain: 'studenthub-uploads-dev-server',
     serviceWorker: false
