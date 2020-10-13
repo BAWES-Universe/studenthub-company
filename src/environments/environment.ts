55d6de3f916cc389152704bb4dd4360d68a82a3a
Krushn Kathrecha <kathrechakrushn@gmail.com>
2020-10-13 19:37:45 +0530

    use optimized resources for cloudinary

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   envName: 'dev',
   permanentBucketUrl: "https://studenthub-uploads-dev-server.s3.amazonaws.com/",
-  cloudinaryUrl: 'https://res.cloudinary.com/studenthub/image/upload/c_thumb,w_200,h_200,g_face/v1596525812/candidate-photo/dev/',
+  cloudinaryUrl: 'https://res.cloudinary.com/studenthub/image/upload/c_thumb,w_200,h_200,g_face,q_auto:low/v1596525812/candidate-photo/dev/',
   apiEndpoint: 'https://employer.api.dev.studenthub.co/v1',
   environmentName: 'Production Server',
   serviceWorker: false
