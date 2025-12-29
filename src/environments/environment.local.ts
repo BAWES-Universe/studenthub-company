9a8d3875e38122adbc709c3e13f2c92a86ef734e
Khalid Al-Mutawa <khalid@bawes.net>
2025-12-29 18:52:22 +0300

    Add local env for new docker setup

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+    production: false,
+    envName: 'local',
+    algoliaCandidateIndex: 'dev_candidate_public',
+    algoliaCacheDuration: 5 * 60 * 1000, // 5 min in millisecond
+    apiEndpoint: 'http://company.studenthub.local/v1',
+    permanentBucketUrl: "https://studenthub-uploads-dev-server.s3.amazonaws.com/",
+    cloudinaryUrl: 'https://res.cloudinary.com/studenthub/image/upload/c_thumb,w_200,h_200,g_face,q_auto:low/v1596525812/dev/',
+    environmentName: 'Local Server',
+    serviceWorker: true,
+    mixpanelKey: 'ac62dbe81767f8871f754c7bdf6669d6',
+};
